1ae884b693f6b22438707d20b56aee32b5d94d81
Ameya Deolalkar <deolalkar.ameya192@gmail.com>
2015-09-30 07:15:39 -0400

    changes 9/30

--- Restaurant Reservation/public/services/authService.js
+++ Restaurant Reservation/public/services/authService.js
@@ -8,31 +8,54 @@ angular.module('authService', [])
 // ===================================================
 .factory('Auth', function($http, $scope) {
 
-  // create auth factory object
-  var authFactory = {};
+// create auth factory object
+var authFactory = {};
 
-  // log a user in
-  authFactory.login = function(email, password) {
+// log a user in
+authFactory.login = function(email, password) {
 
-    // return the promise object and its data
-    return $http.post('/api/login',$scope.user) {
-      email : email,
-      password: password
-    })
-      .success(function(data,status) {
-        console.log(status)
-        return data;
-      });
+  // return the promise object and its data
+  return $http.post('/api/login',$scope.user) {
+    email : email,
+    password: password
+  })
+    .success(function(data,status) {
+    console.log(status)
+    return data;
+  });
 
 authFactory.logout = function(){
   $http.post('/logout')
 }
 
-authFactory.isLoggedIn = function(){
-  
+authFactory.isLoggedIn = function($q, $timeout, $http, $location, $rootScope){
+  var deferred = $q.defer();
+  $http.get('/api/loggedin').success(function(user){
+    if(user !== 0)
+      deferred.resolve();
+
+    else{
+      $rootScope.message = 'You need to log in';
+      deferred.reject();
+      $location.url('/api/login');
+    }
+  });
+  return deferred.promise
 }
 
-  }
+$httpProvider.interceptors.push(function($q, $location) {
+      return {
+        response: function(response) {
+          // do something on success
+          return response;
+        },
+        responseError: function(response) {
+          if (response.status === 401)
+            $location.url('/login');
+          return $q.reject(response);
+        }
+      };
+    });
   };
 
 
