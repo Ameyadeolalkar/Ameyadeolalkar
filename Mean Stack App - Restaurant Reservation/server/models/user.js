f6bc082b627937b7e8280d9236e2b7505ac6631e
sbhat90 <sbhat90@users.noreply.github.com>
2015-10-04 18:38:32 -0400

    Update user.js

--- Mean Stack App - Restaurant Reservation/server/models/user.js
+++ Mean Stack App - Restaurant Reservation/server/models/user.js
@@ -3,11 +3,22 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
 
-
-var User = new Schema({
-  username: String,
-  password: String
+var User   = new Schema({
+	Firstname: String,
+	Lastname: String,
+	email: { type: String, required: true, index: { unique: true }},
+	password: { type: String, required: true, select: false },
+	type: String,
+	phoneNumber: String,
+	Reservation: [{
+		Reservation_ID: String,
+		ReservationSlot: String,
+		TableNumber: Number,
+		NumberOfGuests: Number,
+		ReservationDate: String,
+	}]
 });
+
 
 User.plugin(passportLocalMongoose);
 
